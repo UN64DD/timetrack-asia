0c9e35c846377ac87ca2e359bdaa8a74d7698069
UN64DD <azhar.janius2005@gmail.com>
2026-04-29 21:53:30 +0800

    new branch update

--- time-track (4)/src/lib/sounds.ts
+++
@@ -1,8 +0,0 @@
-
-const tapSound = new Audio('/src/assets/tap.mp3');
-
-export const playTap = () => {
-  tapSound.currentTime = 0;
-  tapSound.volume = 0.3;
-  tapSound.play().catch(e => console.warn('Audio playback failed:', e));
-};
